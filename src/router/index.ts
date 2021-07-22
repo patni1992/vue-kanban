96a3ee3005081b387ce0725e6c25de08ddc9afb4
Patrik Nilsson <patriknilsson1992@gmail.com>
2021-07-22 21:45:14 +0200

    Creates public & protected routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,59 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Login from '@/views/auth/Login.vue'
+import Register from '@/views/auth/Register.vue'
 import Board from '@/views/board/Board.vue'
+import VerifyEmail from '@/views/auth/VerifyEmail.vue'
+import useAuth from '@/store/useAuth'
 
 const routes: Array<RouteRecordRaw> = [
   {
+    path: '/verify-email',
+    name: 'VerifyEmail',
+    component: VerifyEmail,
+    meta: {
+      isPublic: true,
+    },
+  },
+  {
     path: '/login',
-    name: 'Home',
+    name: 'Login',
     component: Login,
+    meta: {
+      isPublic: true,
+    },
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+    meta: {
+      isPublic: true,
+    },
   },
   {
     path: '/boards',
     name: 'Board',
     component: Board,
   },
-
   {
     path: '/',
-    redirect: '/login',
+    name: 'Home',
+    component: Board,
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = useAuth().isAuthenticated.value
+
+  if (!to.meta.isPublic && !isAuthenticated) return next({ name: 'Login' })
+  else if (to.meta.isPublic && isAuthenticated) return next({ name: 'Board' })
+
+  next()
 })
 
 export default router
